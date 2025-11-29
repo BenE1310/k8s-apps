58134765a4158478e1164b912c02d6005b17b520
bene1310 <beneytan5@gmail.com>
2025-11-29 12:45:04 +0200

    Add PostgreSQL

--- fastapi_mongo_app/database.py
+++
@@ -1,5 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-
-MONGO_URI = "mongodb://mongo-0.mongo,mongo-1.mongo,mongo-2.mongo:27017/?replicaSet=rs0"
-client = AsyncIOMotorClient(MONGO_URI)
-db = client["filtrafix"]  # Choose your DB name
--- fastapi_mongo_app/models.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field
-from typing import Optional
-
-class Report(BaseModel):
-    title: str
-    description: Optional[str] = None
-    reporter: Optional[str] = None
